52dd15cff50cf960dad68b77bb371fb4c75fb262
Tom Janke <info@tomjanke.de>
2020-12-18 00:36:50 +0100

    Remove options resolver in favor of named arguments

--- src/Attribute/CmsEntity.php
+++ src/Attribute/CmsEntity.php
@@ -4,23 +4,14 @@
 namespace MelodyNG\Core\Attribute;
 
 use Attribute;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 #[Attribute(Attribute::TARGET_CLASS)]
 class CmsEntity
 {
 
-  public ?string $label;
-
-  public function __construct(array $options = []) {
-    $resolver = new OptionsResolver();
-
-    $resolver->setDefault("label", null);
-    $resolver->setAllowedTypes("label", ["string", "null"]);
-
-    [
-      "label" => $this->label
-    ] = $resolver->resolve($options);
+  public function __construct(
+    public ?string $label = null
+  ) {
   }
 
 }
--- src/Attribute/CmsProperty.php
+++ src/Attribute/CmsProperty.php
@@ -4,33 +4,16 @@
 namespace MelodyNG\Core\Attribute;
 
 use Attribute;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class CmsProperty
 {
 
-  public ?string $formType;
-  public array $formOptions;
-  public ?string $group;
-
-  public function __construct(array $options = []) {
-    $resolver = new OptionsResolver();
-
-    $resolver->setDefault("form_type", null);
-    $resolver->setAllowedTypes("form_type", ["string", "null"]);
-
-    $resolver->setDefault("form_options", []);
-    $resolver->setAllowedTypes("form_options", ["array"]);
-
-    $resolver->setDefault("group", null);
-    $resolver->setAllowedTypes("group", ["string", "null"]);
-
-    [
-      "form_type" => $this->formType,
-      "form_options" => $this->formOptions,
-      "group" => $this->group
-    ] = $resolver->resolve($options);
+  public function __construct(
+    public ?string $formType = null,
+    public array $formOptions = [],
+    public ?string $group = null
+  ) {
   }
 
 }
